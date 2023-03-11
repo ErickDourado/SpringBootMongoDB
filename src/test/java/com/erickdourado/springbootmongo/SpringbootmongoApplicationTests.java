0481bbf15540d407ed16575e9f607a29d2906cde
Erick <erickdourado_@hotmail.com>
2023-03-11 14:27:38 -0300

    Primeiro commit - projeto criado

---
+++ src/test/java/com/erickdourado/springbootmongo/SpringbootmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erickdourado.springbootmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
