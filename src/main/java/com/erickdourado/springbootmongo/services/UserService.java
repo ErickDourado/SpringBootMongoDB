299ea699f10ff0a60c4d4677ce0c61c733a6046b
Erick <erickdourado_@hotmail.com>
2023-03-11 16:39:39 -0300

    Obtendo um usuário por id

--- src/main/java/com/erickdourado/springbootmongo/services/UserService.java
+++ src/main/java/com/erickdourado/springbootmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.erickdourado.springbootmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.erickdourado.springbootmongo.domain.User;
 import com.erickdourado.springbootmongo.repositories.UserRepository;
+import com.erickdourado.springbootmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,6 +18,11 @@ public class UserService {
 	
 	public List<User> findAll() {
 		return repository.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> user = repository.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 	
 }
