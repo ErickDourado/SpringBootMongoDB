ecd1597fa4b27b30ce64c4df63d64389a924ac4b
Erick <erickdourado_@hotmail.com>
2023-03-15 15:12:11 -0300

    Consulta simples com query methods

--- src/main/java/com/erickdourado/springbootmongo/repositories/PostRepository.java
+++ src/main/java/com/erickdourado/springbootmongo/repositories/PostRepository.java
@@ -1,9 +1,13 @@
 package com.erickdourado.springbootmongo.repositories;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.erickdourado.springbootmongo.domain.Post;
 
 public interface PostRepository extends MongoRepository<Post, String> {
 
+	List<Post> findByTitleContainingIgnoreCase(String title);
+	
 }
